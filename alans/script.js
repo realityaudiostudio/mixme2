10d82a05f26cf09b94fd0533c9bfbb9e49d56faa
Reality Audio Studio Dev <87328737+realityaudiostudio@users.noreply.github.com>
2022-08-12 18:29:10 +0530

    Times of Melodica

---
+++ alans/script.js
@@ -0,0 +1,20 @@
+var titlearray =["css","js","python","java","android","jquery","ruby"];
+var descriptionarray =["css style","js program","python code","java objects","android program","jquery objects","ruby code"];
+var webarray =["https://realityaudiostudio.github.io","https://realityaudiostudio.github.io/simzmly","https://realityaudiostudio.github.io","https://realityaudiostudio.github.io","https://realityaudiostudio.github.io","https://realityaudiostudio.github.io","https://realityaudiostudio.github.io"];
+
+
+var dynamic = document.querySelector('.container');  
+for (var i = 0; i < titlearray.length; i++) {
+  var fetch = document.querySelector('.container').innerHTML;  
+  dynamic.innerHTML = `<div id="cards${i}" class="boxes">
+      <div class="box-content">
+        <h2>${titlearray[i]}</h2>
+        <p>
+          ${descriptionarray[i]}
+        </p>
+        <a class="showmore" href="${webarray[i]}">ReadMore</a>
+      </div>
+    </div>` + fetch ; 
+    var bgimg = document.getElementById(`cards${i}`);
+    bgimg.style.backgroundImage = `url('img/${titlearray[i]}.jpg')`;
+} 
